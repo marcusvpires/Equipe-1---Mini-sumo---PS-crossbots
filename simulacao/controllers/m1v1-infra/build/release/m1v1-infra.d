2fe0e78f4fc2e3b4ee815947ccec429bba854482
marcusvpires <marcus.vinicius.mvap@gmail.com>
2022-10-28 16:36:54 -0300

    a

--- simulacao/controllers/m1v1-infra/build/release/m1v1-infra.d
+++
@@ -1,6 +0,0 @@
-build/release/m1v1-infra.o: m1v1-infra.cpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/marcu/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
