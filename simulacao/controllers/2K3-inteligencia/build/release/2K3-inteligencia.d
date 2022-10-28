9f2f36ed4cd35dc28c8e561493a53b49cb0d34c0
marcusvpires <marcus.vinicius.mvap@gmail.com>
2022-10-28 16:33:11 -0300

    apagando lixo

--- simulacao/controllers/2K3-inteligencia/build/release/2K3-inteligencia.d
+++
@@ -1,6 +0,0 @@
-build/release/2K3-inteligencia.o: 2K3-inteligencia.cpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/marcu/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
--- simulacao/controllers/3K3-testes/build/release/3K3-testes.d
+++
@@ -1,6 +0,0 @@
-build/release/3K3-testes.o: 3K3-testes.cpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/marcu/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
