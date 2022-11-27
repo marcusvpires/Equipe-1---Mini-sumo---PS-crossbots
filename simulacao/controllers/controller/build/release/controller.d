7c3b35f73e142ce2b33179cdb09eff20a6006493
marcusvpires <marcus.vinicius.mvap@gmail.com>
2022-11-27 04:08:08 -0300

    simplificando

---
+++ simulacao/controllers/controller/build/release/controller.d
@@ -0,0 +1,6 @@
+build/release/controller.o: controller.cpp \
+ C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/marcu/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
