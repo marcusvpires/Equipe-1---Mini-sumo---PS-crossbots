d5c02d0424e005cae6cc1fa6d56deb646df4287e
marcusvpires <marcus.vinicius.mvap@gmail.com>
2022-12-01 17:21:46 -0300

    sensor de linha

--- simulacao/controllers/1v0-motor-e-sensor-distancia/1v0-motor-e-sensor-distancia.c
+++ simulacao/controllers/1v0-motor-e-sensor-distancia/1v0-motor-e-sensor-distancia.c
@@ -19,6 +19,7 @@
  *               and avoids the walls
  */
 
+#include <stdio.h>
 #include <webots/distance_sensor.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
@@ -32,8 +33,12 @@ int main() {
   /* Get and enable the distance sensors. */
   WbDeviceTag ds0 = wb_robot_get_device("ds0");
   WbDeviceTag ds1 = wb_robot_get_device("ds1");
+  WbDeviceTag right_ground_ir = wb_robot_get_device("right_ground_infrared");
+  WbDeviceTag left_ground_ir = wb_robot_get_device("left_ground_infrared");
   wb_distance_sensor_enable(ds0, TIME_STEP);
   wb_distance_sensor_enable(ds1, TIME_STEP);
+  wb_distance_sensor_enable(right_ground_ir, TIME_STEP);
+  wb_distance_sensor_enable(left_ground_ir, TIME_STEP);
 
   /* get a handler to the motors and set target position to infinity (speed control). */
   WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
@@ -44,25 +49,21 @@ int main() {
   wb_motor_set_velocity(right_motor, 0.0);
 
   while (wb_robot_step(TIME_STEP) != -1) {
-    /* Get distance sensor values */
     double ds0_value = wb_distance_sensor_get_value(ds0);
     double ds1_value = wb_distance_sensor_get_value(ds1);
 
-    /* Compute the motor speeds */
+    // sensor de linha ( 952 para preto e 1024 para branco )
+    double right_ground_ir_value = wb_distance_sensor_get_value(right_ground_ir);
+    double left_ground_ir_value = wb_distance_sensor_get_value(left_ground_ir);
+
     double left_speed, right_speed;
     if (ds1_value > 500) {
-      /*
-       * If both distance sensors are detecting something, this means that
-       * we are facing a wall. In this case we need to move backwards.
-       */
+
       if (ds0_value > 500) {
         left_speed = -SPEED;
         right_speed = -SPEED / 2;
       } else {
-        /*
-         * We turn proportionnaly to the sensors value because the
-         * closer we are from the wall, the more we need to turn.
-         */
+
         left_speed = -ds1_value / 100;
         right_speed = (ds0_value / 100) + 0.5;
       }
@@ -70,14 +71,15 @@ int main() {
       left_speed = (ds1_value / 100) + 0.5;
       right_speed = -ds0_value / 100;
     } else {
-      /*
-       * If nothing was detected we can move forward at maximal speed.
-       */
+
       left_speed = SPEED;
       right_speed = SPEED;
     }
 
-    /* Set the motor speeds. */
+    if 
+    printf("ir ground right = %f\n", right_ground_ir_value);
+    printf("ir ground left = %f\n", left_ground_ir_value);
+
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
   }
