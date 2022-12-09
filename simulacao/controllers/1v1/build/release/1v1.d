fe82e84af62acb4d8fa3131b116a2cdf25bd40ee
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-12-09 16:54:30 -0300

    mudando nomes

--- simulacao/controllers/1v1/build/release/1v1.d
+++
@@ -1,6 +0,0 @@
-build/release/1v1.o: 1v1.c \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\marcu\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
